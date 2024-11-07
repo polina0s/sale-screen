d5dcc4f7aca22cabcfe48ffd8fb1da3366638613
polina0s <polina.osintseva000@gmail.com>
2024-11-07 18:19:19 +0400

    fixed layout for header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,14 +5,14 @@ export function Header() {
   return <div className='bg-white w-full h-[79px] flex justify-center items-center'>
     <p className="font-ptRootUi font-bold text-2xl text-gray-400 mr-[15px]">Скидка действует:</p>
     <div className="w-[140px]">
-      <div className="flex justify-between">
+      <div className="flex justify-between px-[7.5px] relative">
         <p className="font-bebasNeue text-4xl leading-4xl text-turquoise-100">00</p>
         <Image src={separator} alt='separator' />
         <p className="font-bebasNeue text-4xl leading-4xl text-turquoise-100">00</p>
       </div>
-      <div className="flex justify-between font-ptRootUi font-bold text-gray-300">
-        <p>минут</p>
-        <p>секунд</p>
+      <div className="flex justify-between font-ptRootUi font-bold text-gray-300 px-[9px] w-[140px] absolute top-[56px]">
+        <p className="leading-[130%]">минут</p>
+        <p className="leading-[130%]"й  >секунд</p>
       </div>
     </div>
   </div>
